66c8f5e38ffb82191271dc576c75a4d3dd963408
Pravallika Reddy <pravallika@Pravallikas-MacBook-Pro.local>
2019-02-07 15:47:24 -0500

    postgres setup

--- services/users/manage.py
+++ services/users/manage.py
@@ -7,6 +7,11 @@ from project import app
 
 cli = FlaskGroup(app)
 
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == '__main__':
     cli()
