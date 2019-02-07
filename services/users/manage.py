7f98644bb4d08c533465b4d57d2b304ddcb84cb6
Pravallika Reddy <pravallika@Pravallikas-MacBook-Pro.local>
2019-02-07 16:32:04 -0500

    restful routes

--- services/users/manage.py
+++ services/users/manage.py
@@ -23,6 +23,13 @@ def test():
         return 0
     return 1
 
+@cli.command('seed_db')
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.commit()
+
 if __name__ == '__main__':
     cli()
 
