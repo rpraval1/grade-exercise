5de5a069db39b37de9a15da81673d3727bbfca30
Pravallika Reddy <pravallika@Pravallikas-MacBook-Pro.local>
2019-02-07 15:54:39 -0500

    test setup

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -6,6 +6,7 @@ class BaseConfig:
     """Base configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
